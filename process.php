1625c3136f0f2e58dfc3e8ba4698f7cb8b89ea33
mklucey97 <mklucey97@gmail.com>
2025-05-10 18:26:15 -0600

    Added Mickey Mouse image element with alt text and newline for output formatting

--- process.php
+++ process.php
@@ -88,6 +88,7 @@ if ($title && $favdrink && $pname && $favfictionalplace && $favrealplace) {
  } else {
 	 // Invalid input
 	 echo "I'm sorry, your input was not valid.\n";
+	 echo '<img src="images/mickeymouse.jpg" alt="Mickey Mouse">' . "\n";
 
 
 
